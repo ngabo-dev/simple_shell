5e20e2eea3c3ce03eb4e996c1e6161f2f6486b5d
ngabo-dev <ngabo470@gmail.com>
2023-12-13 08:29:56 -0800

    task 0

--- betty.c
+++ betty.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - Entry point
+ * main - Simple Shell
  *
  * Return: Always 0
  */
 int main(void)
 {
-    printf("Hello, world!\n");
+    char *buffer = NULL;
+    size_t bufsize = 0;
+    ssize_t characters;
+
+    while (1)
+    {
+        printf("$ ");  /* Display shell prompt */
+
+        /* Read input from the user */
+        characters = getline(&buffer, &bufsize, stdin);
+
+        /* Check for end of file or error */
+        if (characters == -1)
+        {
+            perror("getline");
+            break;
+        }
+
+        /* Print the entered command */
+        printf("You entered: %s", buffer);
+    }
+
+    /* Free the allocated memory */
+    free(buffer);
+
     return 0;
 }
